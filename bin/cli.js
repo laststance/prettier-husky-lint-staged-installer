5da127dde23cf1538ae9d5127550b2e3c6c9a7af
ryota-murakami <dojce1048@gmail.com>
2023-10-11 10:05:42 +0900

    add reflect command

--- bin/cli.js
+++ bin/cli.js
@@ -14,29 +14,34 @@ if (existsSync('package-lock.json')) {
   precommitExec = 'npm lint-staged'
   await $`npm install --save-dev lint-staged prettier`
   await $`npx husky-init && npm install`
-}
-if (existsSync('yarn.lock')) {
+  await $`npm install` // to refrect package.json
+} else if (existsSync('yarn.lock')) {
   precommitExec = 'yarn lint-staged'
   await $`yarn add -D lint-staged prettier`
   await $`yarn dlx husky-init --yarn2`
+  await $`yarn install` // to refrect package.json
 } else if (existsSync('pnpm-lock.yaml')) {
-  precommitExec = 'npm lint-staged'
+  precommitExec = 'pnpm lint-staged'
   await $`pnpm install --save-dev lint-staged prettier`
   await $`pnpm dlx husky-init && pnpm install`
+  await $`pnpm install` // to refrect package.json
 } else if (existsSync('bun.lockb')) {
-  precommitExec = 'npm lint-staged'
+  precommitExec = 'npx lint-staged'
   await $`bun install -D lint-staged prettier`
   await $`bunx husky-init && bun install`
+  await $`bun install` // to refrect package.json
 } else {
   // fallback to npm
   precommitExec = 'npm lint-staged'
   await $`npx husky-init && npm install && npm install --save-dev lint-staged prettier`
+  await $`npm install` // to refrect package.json
 }
 p.stop()
 intro('Configration...')
 p.start()
 execSync('npm pkg set lint-staged.*="prettier --ignore-unknown --write"')
 execSync('npm pkg set scripts.prettier="prettier --ignore-unknown --write ."')
+// replace "npm test" to  in $precommitExec '.husky/pre-commit'
 const path = '.husky/pre-commit'
 readFile(path, 'utf8', (err, data) => {
   if (err) throw err
