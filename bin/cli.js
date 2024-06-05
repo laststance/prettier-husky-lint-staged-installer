d5265a0a186973c5f6ba05400abba98a11cb366d
Ryota Murakami <dojce1048@gmail.com>
2024-06-05 16:48:32 +0900

    Refactor pre-commit script setup in cli.js
    - Renamed variable precommitExec to precommitFileBody for clarity.
    - Updated logic to set precommitFileBody based on the package manager lock file

--- bin/cli.js
+++ bin/cli.js
@@ -10,19 +10,19 @@ const p = spinner()
 intro('Installing via husky, prettier, lint-staged...')
 p.start()
 
-let precommitExec
+let precommitFileBody
 if (existsSync('package-lock.json')) {
-  precommitExec = 'npx lint-staged'
+  precommitFileBody = 'npx lint-staged'
   await $`npm install --save-dev husky lint-staged prettier`
   await $`npx husky init`
   await $`npm install` // to refrect package.json
 } else if (existsSync('pnpm-lock.yaml')) {
-  precommitExec = 'pnpm lint-staged'
+  precommitFileBody = 'pnpm lint-staged'
   await $`pnpm install --save-dev husky lint-staged prettier`
   await $`pnpm exec husky init`
   await $`pnpm install` // to refrect package.json
 } else if (existsSync('bun.lockb')) {
-  precommitExec = 'bunx lint-staged'
+  precommitFileBody = 'bunx lint-staged'
   await $`bun install -D husky lint-staged prettier`
   await $`bunx husky init`
   await $`bun install` // to refrect package.json
@@ -40,10 +40,11 @@ execSync('npm pkg set scripts.prettier="prettier --ignore-unknown --write ."')
 const path = '.husky/pre-commit'
 readFile(path, 'utf8', (err, data) => {
   if (err) throw err
-  // "npm test" written in .husky/pre-commit by default. So replace it to npx|pnpm|bun lint-staged
-  const result = data.replace(/npm test/g, precommitExec)
 
-  writeFile(path, result, 'utf8', (err) => {
+  // overwrite .husky/pre-commit
+  data = precommitFileBody
+
+  writeFile(path, data, 'utf8', (err) => {
     if (err) throw err
   })
 })
